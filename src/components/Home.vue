23f940d3a556948dea6f3cf048b6e005a0bcb8f1
Kevin Neyer <59579209+kevinneyer@users.noreply.github.com>
2021-05-19 16:41:37 -0400

    initial commit. chart shows

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>CHARTS</h1>
+        <line-chart />
+    </div>
+</template>
+
+<script>
+import LineChart from './line-chart.vue'
+
+export default {
+    name: 'Home',
+    components:{
+        LineChart
+    }
+}
+</script> 
---
+++ src/components/line-chart.vue
@@ -0,0 +1,26 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+    name: 'line-chart',
+    extends: Line,
+    data() {
+        return {
+            chartdata: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [
+                    {
+                    label: 'Data One',
+                    backgroundColor: '#f87979',
+                    data: [40, 39, 10, 40, 39, 80, 40]
+                    }
+                ]
+            },
+            options: {responsive: true, maintainAspectRatio: false}
+        }
+    },
+    mounted() {
+        this.renderChart(this.chartdata, this.options)
+    }
+}
+</script>
