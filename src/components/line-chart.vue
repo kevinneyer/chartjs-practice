8c2a4aa529b96851ec60ee9cded5d46105401b83
Kevin Neyer <59579209+kevinneyer@users.noreply.github.com>
2021-05-20 16:37:31 -0400

    took chart data out of component state. Need to do away with tooltips.

--- src/components/line-chart.vue
+++ src/components/line-chart.vue
@@ -4,46 +4,38 @@ import { Line } from 'vue-chartjs'
 export default {
     name: 'line-chart',
     extends: Line,
-    data() {
-        return {
-            chartdata: {
-                labels: ["<3'", "4-5'", "6-10'", "11-15'", "16-20'", "21-30'", "31-50'" ],
+    mounted() {
+        this.renderChart(
+            {
+                labels: ["<3'", "4-5'", "6-10'", "11-15'", "16-20'", "21-30'", "31-50'", "" ],
                 datasets: [
                     {
                     backgroundColor: '#54AE47',
-                    data: [100, 75, 50, 100, 50, 25, 37],
+                    data: [100, 50, 30, 30, 12.5, 10, 10, 0],
                     tension: 0.0
                     }
                 ]
-                // yAxes: {
-                //     labels: [
-                //         '0%', '25%', '50%', '75%', '100%'
-                //     ]
-                // }
-            },
-            options: {
+            }, 
+            {
                 responsive: true, 
                 maintainAspectRatio: false,
                 layout: {
                     padding: 20
                 },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        min: 0,
-                        stepSize: 25,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            min: 0,
+                            stepSize: 25,
 
-                        callback: function(value) {
-                            return value + '%'
+                            callback: function(value) {
+                                return value + '%'
+                            }
                         }
-                    }
-                }]
+                    }]
+                }
             }
-            }
-        }
-    },
-    mounted() {
-        this.renderChart(this.chartdata, this.options)
+        )
     }
 }
 </script>
