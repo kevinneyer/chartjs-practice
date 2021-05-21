4eef47fa6257925b91fbcdb25bc4eeb093aa0d6b
Kevin Neyer <59579209+kevinneyer@users.noreply.github.com>
2021-05-21 14:58:44 -0400

    bar chart started

--- src/components/BarContainer.vue
+++ src/components/BarContainer.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
         Bar Container
+        <bar-chart 
+        :chartData='chartData'/>
     </div>
 </template>
 
 <script>
+import BarChart from './bar-chart.vue'
+
 export default {
-    name: 'BarContainer'
+    name: 'BarContainer',
+    components: {
+        BarChart
+    },
+    data() {
+        return { 
+            chartData: {
+                labels: ['Good Lie', 'Bad Lie', 'No Shot', 'Penalty', 'OB/Lost'],
+                data: [3.1, 3.1, 3.1, 3.1, 3.1]
+            }
+        }
+ 
+    }
 }
 </script>
---
+++ src/components/bar-chart.vue
@@ -0,0 +1,50 @@
+<script>
+
+import { Bar } from 'vue-chartjs'
+
+export default {
+    name: 'bar-chart',
+    extends: Bar,
+    props:['chartData'],
+    mounted() {
+        this.renderChart(
+            {
+                labels: this.chartData.labels,
+                datasets: [
+                    {
+                    label: 'You',
+                    data: this.chartData.data,
+                    backgroundColor: '#54AE47'
+                    },
+                    {
+                    label: '0-2 Handicap',
+                    data: [5,5,5,5,5],
+                    backgroundColor: 'rgba(234, 138, 59, 1)'
+                    }
+                ]
+            },
+            {
+                responsive: true, 
+                maintainAspectRatio: false,
+                layout: {
+                    padding: 20
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            min: 0,
+                            stepSize: 2,
+                            
+                            callback: function(value) {
+                                if (typeof(value) === 'number'){
+                                    return " "
+                                }
+                            }
+                        }
+                    }]
+                },
+            }
+            )
+    }
+}
+</script>
